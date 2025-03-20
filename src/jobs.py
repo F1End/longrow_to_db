6f8f2d15a20b9bfe19d91ce2b80485518fd2dd20
F1End <horvirich@gmail.com>
2025-03-20 21:04:07 +0100

    Slight modification for jobs and gitignore fix

---
+++ src/jobs.py
@@ -0,0 +1,14 @@
+"""
+Actual job definition, preferably based on abstract class from sparkutil
+"""
+
+# clean data (whitespaces)
+
+
+# build class data
+
+
+# build img data
+
+
+# build loss data
