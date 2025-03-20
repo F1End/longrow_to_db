328d2dac4235e88d25e2b498daad5914b91b13c4
F1End <horvirich@gmail.com>
2025-03-20 18:52:51 +0100

    Adding high level design

---
+++ src/sparkutil.py
@@ -0,0 +1,12 @@
+"""
+Spark specific utility and abstractions
+"""
+
+
+# class for holding/governing/getting Spark session
+
+# Abstract class for spark jobs to be implemented
+# Input is spark session, data(frame?) and cols to keep/remove
+
+
+# Class for column addition/removal request
