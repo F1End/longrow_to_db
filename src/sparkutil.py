97055329c8b6d416c34b9d53b78db1fd434ffe0e
F1End <horvirich@gmail.com>
2025-03-21 20:58:41 +0100

    Added requirements.txt and small work on session builder and ETL class

--- src/sparkutil.py
+++ src/sparkutil.py
@@ -1,11 +1,35 @@
 """
 Spark specific utility and abstractions
 """
-
+from abc import ABC, abstractmethod
+from pyspark.sql.session import SparkSession
 
 # class for holding/governing/getting Spark session
+def create_spark_session(appname: str, config: dict) -> SparkSession:
+    builder = SparkSession.builder.appName(appname)
+    for key, value in configs.items():
+        builder = builder.config(key, value)
+
+    return builder.getOrCreate()
 
 # Abstract class for spark jobs to be implemented
+class ETL(ABC):
+    def __init__(self, source):
+        self.source = None
+        self.data = None
+
+    @abstractmethod
+    def load(self):
+        pass
+
+    @abstractmethod
+    def transform(self):
+        pass
+
+    @abstractmethod
+    def load(self):
+        pass
+
 # Input is spark session, data(frame?) and cols to keep/remove
 
 
