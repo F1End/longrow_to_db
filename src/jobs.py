1038e058d1123842a27581803adfcbb3a99efe2a
F1End <horvirich@gmail.com>
2025-03-26 22:48:18 +0100

    Adding some initial work on sparkutil: -ETL abstract class -spark session builder -etl job class -trimming function
    on jobs:
    -Oryx loss items job
    -Oryx proof (urls) job
    -Starting on ETLExecutor and summary job

--- src/jobs.py
+++ src/jobs.py
@@ -2,13 +2,94 @@
 Actual job definition, preferably based on abstract class from sparkutil
 """
 
-# clean data (whitespaces)
+from src.sparkutil import ETL, ETLJob, trim_df
+
+ETLJOBS = [OryxLossesItem, OryxLossesProofs, OryxLossesSummary]
 
 
-# build class data
+# etl executor
+class ETLExecutor:
+    def __init__(self):
+        raise NotImplemented
+
+    # method to load ETL jobs
+
+    # method to run single ETL job
+
+    # method to run all ETL jobs one after another
 
 
-# build img data
+class OryxLossesItem(ETL):
+    def __init__(self, source, spark):
+        super().__init__(source, spark)
+
+    def extract(self):
+        self.data = spark.read.option("header", "true").option("inferSchema", "true").csv(self.source)
+
+    def transform(self):
+        self._trim_df()
+        self._filter_base_cols()
+        self._build_cleaned_items()
+        self._split_to_losses()
+        self._split_loss_to_rows()
+        self._filter_final_cols()
+
+    def load(self, path: Union[Path, str]):
+        self.data.write.option("header", True).mode("overwrite").csv(path)
+
+    def _trim_df(self):
+        self.data = trim_df(self.data)
+
+    def _filter_base_cols(self):
+        self.data = self.data.select("category_name", "type_name", "type_ttl_count", "loss_item")
+
+    def _build_cleaned_items(self):
+        """
+        Moving descriptions with multiple losses into more processable, comma delimited text
+        """
+        self.data = (self.data.withColumn("loss_item", regexp_replace(self.data["loss_item"], r"[()]", ""))
+                     .withColumn("cleaned_items", regexp_replace(self.data["loss_item"], "and", ","))
+                     .withColumn("cleaned_items", regexp_replace(self.data["cleaned_items"], r"\b(and)\b", "")))
+
+    def _split_to_losses(self):
+        """
+
+        """
+        self.data = (self.data.withColumn("loss_id", split(self.data["cleaned_items"], ",\s*"))
+                     .withColumn("loss_type", regexp_replace(self.data["loss_item"], r".*?,\s*", "")))
+
+    def _split_loss_to_rows(self):
+        """
+        Separating ids into multiple rows and removing
+        remaining invalid rows (removing those without integer id)
+        """
+        self.data = (self.data.withColumn("loss_id", explode(self.data["loss_id"]))
+                     .withColumn("loss_id", col("loss_id").cast("int"))
+                     .filter(col("loss_id").isNotNull()))
+
+    def _filter_final_cols(self):
+        self.data = self.data.select("category_name", "type_name", "loss_item",
+                                     "cleaned_items", "loss_id", "loss_type")
 
 
-# build loss data
+class OryxLossesProofs(ETL):
+    def __init__(self, source, spark):
+        super().__init__(source, spark)
+
+    def extract(self):
+        self.data = spark.read.option("header", "true").option("inferSchema", "true").csv(self.source)
+
+    def transform(self):
+        self._trim_df()
+        self.data = self.data.select("loss_proof").distinct()
+
+    def load(self, path: Union[Path, str]):
+        self.data.write.option("header", True).mode("overwrite").csv(path)
+
+    def _trim_df(self):
+        self.data = trim_df(self.data)
+
+
+class OryxLossesSummary(ETL):
+    def __init__(self):
+        raise NotImplemented
--- src/sparkutil.py
+++ src/sparkutil.py
@@ -2,7 +2,12 @@
 Spark specific utility and abstractions
 """
 from abc import ABC, abstractmethod
+from typing import Union
+from pathlib import Path
+
 from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import trim
+
 
 # class for holding/governing/getting Spark session
 def create_spark_session(appname: str, config: dict) -> SparkSession:
@@ -14,12 +19,13 @@ def create_spark_session(appname: str, config: dict) -> SparkSession:
 
 # Abstract class for spark jobs to be implemented
 class ETL(ABC):
-    def __init__(self, source):
-        self.source = None
+    def __init__(self, source: Union[Path, str], sparks_ession):
+        self.source = source
+        self.sparks_ession = sparks_ession
         self.data = None
 
     @abstractmethod
-    def load(self):
+    def extract(self):
         pass
 
     @abstractmethod
@@ -30,7 +36,21 @@ class ETL(ABC):
     def load(self):
         pass
 
+
+# ETL job, including input and output
+class ETLJob:
+    def __init__(self, etl_logic: ETL, source: Union[Path, str], output: Union[Path, src], extra_args: dict):
+        self.etl = etl_logic
+        self.source = source
+        self.output = output
+        self.args = extra_args
+
 # Input is spark session, data(frame?) and cols to keep/remove
 
 
-# Class for column addition/removal request
+def trim_df(spark_df):
+    """
+    Removing whitespace from start/end of all columns
+    """
+    trimmed_df = spark_df.select([trim(sparkdf[col]).alias(col) for col in spark_df.columns])
+    return trimmed_df
