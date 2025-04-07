929ac203d22efb9cbd29cf4d9042fca555b6079f
F1End <horvirich@gmail.com>
2025-04-07 18:06:47 +0200

    Updated main with config parsing and cleaned up imports

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from src.util import parse_args, parse_yaml, init_logging
 from src.executor import ETLExecutor
-from src.sparkutil import config
-from src.db_tools import BuildDB, DBConn
+from src.db_tools import DBConn
+
 
 if __name__ == "__main__":
     args = parse_args()
     init_logging(args.debug)
     job_config = parse_yaml(args.job_config)
 
+    if args.base_config:
+        config = parse_yaml(args.base_config)["configs"][args.base_config_key]
+
     if args.init_db:
         db_schema = parse_yaml(args.init_db)
         db_conn = DBConn(db_path=args.db_path)
         with db_conn as db_session:
             db_session.build_db(db_schema)
-        # BuildDB(path=args.db_path, schema=db_schema).run()
 
     if args.db_path:
         job_config["db"] = args.db_path
